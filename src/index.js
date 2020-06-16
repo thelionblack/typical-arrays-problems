a81a756e3013609e4dd2a6781256a4a57dbb746c
Maksim Storazhau <Thelionblack.com@gmail.com>
2020-06-16 09:14:00 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if(!array || array.length <= 0){
+    return 0;
+  }
+  for (i = 0; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i]
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if(!array || array.length <= 0){
+    return 0;
+  }
+  for (i = 0; i < array.length; i++){
+    if(array[i] > max){
+      max = array[i]
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  if(!array || array.length <= 0){
+    return 0;
+  }
+  for (i = 0; i < array.length; i++){
+    avg += array[i];
+  }
+  return avg / array.length;
 }
